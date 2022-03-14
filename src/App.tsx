7151bdcaab34f3f6ad05f08fff80de6d6d791c7d
Jędrek Dąbrowski <jedrzejdabrowski.jd@gmail.com>
2022-03-14 10:55:39 +0100

    🎉 project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <input />
+      <button>SZYFROWANIE BRRR</button>
+      <p>Wynik:</p>
+      <p>{2 + 4}</p>
+    </div>
+  );
+}
+
+export default App;
