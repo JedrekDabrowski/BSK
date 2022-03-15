679060859f12fbfd54bb2ae92a3eaa12015dbb5a
[Kamil Zalewski] <[k.zalewski18@student.pb.edu.pl]>
2022-03-15 14:08:03 +0100

    rail fence decryption implementation

--- src/ciphers/RailFenceCipher.ts
+++ src/ciphers/RailFenceCipher.ts
@@ -1,7 +1,10 @@
 const UP: string = 'UP';
 const DOWN: string = 'DOWN';
+const DEFAULT_TEXT_FOR_ENCRYPTION: string = 'KAMILSLIMAK';
+const DEFAULT_TEXT_FOR_DECRYPTION: string = 'KLMAISIAMLK';
+const DEFAULT_KEY: number = 3;
 
-export const encrypt = (text: string = 'DEFAULT', key: number = 3): string => {
+export const encrypt = (text: string = DEFAULT_TEXT_FOR_ENCRYPTION, key: number = DEFAULT_KEY): string => {
     /* matrix initialization
      *  rows: key
      *  columns: text.length
@@ -30,6 +33,10 @@ export const encrypt = (text: string = 'DEFAULT', key: number = 3): string => {
         else row--;
     }
 
+    /*
+     * reading encrypted text
+     */
+
     for (let i: number = 0; i < key; i++) {
         for (let j: number = 0; j < text.length; j++) {
             if (matrix[i][j] !== '*') encryptedText = encryptedText.concat(matrix[i][j]);
@@ -39,6 +46,64 @@ export const encrypt = (text: string = 'DEFAULT', key: number = 3): string => {
     return encryptedText;
 };
 
-export const decrypt = (cipher: string, key: number): string => {
-    return 'null';
+export const decrypt = (cipher: string = DEFAULT_TEXT_FOR_DECRYPTION, key: number = DEFAULT_KEY): string => {
+    /* matrix initialization
+     *  rows: key
+     *  columns: cipher.length
+     */
+    let matrix = new Array(key).fill('*').map(() => new Array(cipher.length).fill('*'));
+
+    let direction: string = UP;
+    let column: number = 0;
+    let row: number = 0;
+    let indexLetterInCipher: number = 0;
+    let decryptedText: string = '';
+
+    /*
+     * inserts markers in matrix
+     */
+
+    for (let i: number = 0; i < cipher.length; i++) {
+        /*
+         * direction change depends
+         * on whether we reached top/bottom
+         * of the matrix
+         */
+        if (row === 0) direction = DOWN;
+        else if (row === key - 1) direction = UP;
+
+        /*
+         *  inserts marker
+         */
+        matrix[row][column++] = '#';
+
+        if (direction === DOWN) row++;
+        else row--;
+    }
+
+    /*
+     * inserts letters of cipher where markers are set
+     */
+    for (let i: number = 0; i < key; i++)
+        for (let j: number = 0; j < cipher.length; j++)
+            if (matrix[i][j] === '#' && indexLetterInCipher < cipher.length)
+                matrix[i][j] = cipher[indexLetterInCipher++];
+
+    row = 0;
+    column = 0;
+
+    /*
+     * reading decrypted cipher
+     */
+    for (let i: number = 0; i < cipher.length; i++) {
+        if (row === 0) direction = DOWN;
+        else if (row === key - 1) direction = UP;
+
+        if (matrix[row][column] !== '*') decryptedText = decryptedText.concat(matrix[row][column++]);
+
+        if (direction === DOWN) row++;
+        else row--;
+    }
+
+    return decryptedText;
 };
