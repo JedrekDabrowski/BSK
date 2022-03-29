59d634254a4ce55e86297add13b5d49652f7ce87
[Kamil Zalewski] <[k.zalewski18@student.pb.edu.pl]>
2022-03-29 11:22:44 +0200

    working(? )lfsr

--- src/utils/lfsr.ts
+++ src/utils/lfsr.ts
@@ -1,22 +1,28 @@
-const a = '1-4';
-export const generateLFSRKey = (polynomial: string = a) => {
-    const key = polynomial.split('-').map((a) => parseInt(a));
-    const max = Math.max(...key);
-    let startSeqeunce = '';
-    let lsft = '';
-    for (let i = 0; i < max; i++) {
-        startSeqeunce += Math.random().toFixed();
-    }
-    console.log(startSeqeunce);
-    let xor;
-    for (let i = 0; i < 5; i++) {
-        a: for (let j = 0; j < key.length; j++) {
-            if (j + 1 >= key.length) break a;
+const TEST_POLYNOMNIAL = '1-4';
 
-            xor = parseInt(startSeqeunce[key[j]]) ^ parseInt(startSeqeunce[key[j + 1]]);
+export const generateLFSRKey = (polynomial: string = TEST_POLYNOMNIAL) => {
+    const key:number[] = polynomial.split('-').map((num) => parseInt(num)-1);
+    const maxPower:number = Math.max(...key) + 1;
+    let randomStartSeqeunce:string = '';
+    let lfsr:string = '';
+    let xor;
+
+    for (let i = 0; i < maxPower; i++) {
+        randomStartSeqeunce += Math.random().toFixed();
+    }
+    for (let i = 0; i < 7; i++) {
+        a: for (let j = 0; j < key.length; j++) {
+            if (j + 1 > key.length) break a;
+
+            if(j===0){
+                xor = parseInt(randomStartSeqeunce[key[j]]) ^ parseInt(randomStartSeqeunce[key[j + 1]]);
+            } else if(j>1){
+                xor = parseInt(randomStartSeqeunce[key[j]]) ^ xor);
+            }
+
         }
-        startSeqeunce = xor + startSeqeunce.slice(0, startSeqeunce.length - 1);
-        console.log(startSeqeunce);
+        lfsr = lfsr.concat(randomStartSeqeunce[randomStartSeqeunce.length-1]);
+        randomStartSeqeunce = xor + randomStartSeqeunce.slice(0, randomStartSeqeunce.length - 1);
     }
 };
-generateLFSRKey(a);
+generateLFSRKey(TEST_POLYNOMNIAL);
